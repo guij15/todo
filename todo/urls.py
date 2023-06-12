9175f36f26b74d3959681e36e176a23ffb46d407
guij15 <1310463763@qq.com>
2023-06-12 07:31:38 -0700

    todo app final

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    path('delete/<int:pk>/', views.delete, name='delete'),
+    path('edit/<int:pk>/', views.edit, name='edit'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -7,3 +7,35 @@ def addTask(request):
     task=request.POST['task']
     Task.objects.create(task=task)
     return redirect('home')
+
+def mark_as_done(request,pk):
+    task=Task.objects.get(pk=pk)
+    task.is_completed=True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request,pk):
+    task=Task.objects.get(pk=pk)
+    task.is_completed=False
+    task.save()
+    return redirect('home')
+
+def delete(request,pk):
+    task=Task.objects.get(pk=pk)
+    task.delete()
+    return redirect('home')
+
+def edit(request,pk):
+    get_task=Task.objects.get(pk=pk)
+    if request.method=='POST':
+        task=request.POST['task']
+        get_task.task=task
+        get_task.save()
+        return redirect('home')
+    else:
+        context={
+            'get_task':get_task,
+        }
+
+    return render(request,'edit.html',context)
+
